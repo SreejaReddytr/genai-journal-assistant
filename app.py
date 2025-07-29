bb2b357f5bb349a7ecbf6ecb8978a30603bdf406
SreejaReddytr <158329960+SreejaReddytr@users.noreply.github.com>
2025-07-29 10:27:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
+
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.agents import Tool, initialize_agent, AgentType
